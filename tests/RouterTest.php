c3a93689faf016c5d91a3fcb4c2ae2c68e4bd83f
Abimael Martell <abimex@gmail.com>
2014-03-13 16:52:34 -0700

    add tests directory and phpunit.xml

---
+++ tests/RouterTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class RouterTest extends PHPUnit_Framework_TestCase
+{
+	public function testAdd(){
+		$this->assertTrue(true);
+	}
+
+	public function testDispatch(){
+		$this->assertTrue(true);
+	}
+}
