2001ee2e9362e1903e62ed6a7eb2621b61cc6103
Abimael Martell <abimex@gmail.com>
2014-03-19 00:42:34 -0700

    add integration tests for Router

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -1,12 +1,41 @@
 <?php
+include "vendor/autoload.php";
 
+use SampleVendor\Interview\Router;
+
+/**
+ * Integration tests for Router class
+ */
 class RouterTest extends PHPUnit_Framework_TestCase
 {
-	public function testAdd(){
-		$this->assertTrue(true);
+	public function testRouteNotFound()
+	{
+		$this->setExpectedException('SampleVendor\Interview\Exception\InterviewException');
+
+		$router = new Router;
+
+		$router->dispatch('/');
 	}
 
-	public function testDispatch(){
-		$this->assertTrue(true);
+	public function testAddedRoute()
+	{
+		$router = new Router;
+		$router->add('/', function() {
+			return "such hello!";
+		});
+
+		$this->assertEquals($router->dispatch('/'), 'such hello!');
+	}
+
+	public function testRouteWithParams()
+	{
+		$router = new Router;
+
+		$router->add('/very', function($params) {
+			return isset($params[0]) ? $params[0] : NULL;
+		});
+
+
+		$this->assertEquals($router->dispatch('/very/test'), 'test');
 	}
 }
